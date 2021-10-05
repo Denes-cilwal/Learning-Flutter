4d485ec97b56480b509f91b0d3071edf4eb0ddb2
Denes-cilwal <dinesh.silwal@wesionary.team>
2021-10-05 09:19:55 +0545

    container widget

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,25 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/app_screens/first_screen.dart';
 
-void main() => runApp(const App());
+// void main() => runApp(const App());
 
-class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
+// class App extends StatelessWidget {
+//   const App({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'My Flutter App',
-        home: Scaffold(
-            appBar: AppBar(
-              title: const Text(
-                'My First App Title',
-                style: TextStyle(
-                  color: Colors.brown,
-                ),
-              ),
-            ),
-            body: const FirstScreen()));
-  }
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         debugShowCheckedModeBanner: false,
+//         title: 'My Flutter App',
+//         home: Scaffold(
+//             appBar: AppBar(
+//               title: const Text(
+//                 'My First App Title',
+//                 style: TextStyle(
+//                   color: Colors.brown,
+//                 ),
+//               ),
+//             ),
+//             body: const FirstScreen()));
+//   }
+// }
+
+void main() {
+  runApp(MaterialApp(
+    title: "Exploring UI widgets",
+    home: Home(),
+  ));
 }
